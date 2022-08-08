14fec4276ccc87348129c98a53d64035beac1376
hugo landaverde <landaverde.hugo90@gmail.com>
2022-08-08 03:54:56 +0000

    Finish custom template tags

--- blango/settings.py
+++ blango/settings.py
@@ -75,7 +75,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries':{
+                'blogs_extras': 'blog.templatetags.blogs_extras',
+            }
         },
+        
     },
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -7,3 +7,7 @@ def index(request):
     posts = Post.objects.all() 
     #server is behind 5 hours, cannot filter by timezone
     return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
